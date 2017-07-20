d88b22dbc5c1f9164242701fc0afe810aafde940
ctyvx784 <ctyvx784@gmail.com>
2017-07-20 20:51:14 +0900

    싱글턴 패턴 생성

--- BaseballProject/src/project/baseball/dao/SearchDAO.java
+++ BaseballProject/src/project/baseball/dao/SearchDAO.java
@@ -1,16 +1,27 @@
 package project.baseball.dao;
 
 public class SearchDAO {
+	// 싱글턴 패턴--------------------------------------
+	private static SearchDAO dao = new SearchDAO();
+
+	private SearchDAO() {
+		}
+
+	public static SearchDAO getInstance() {
+		return dao;
+	}
+
+	// ----------------------------------------------
 	public void selectHomeRun() {
-		
+
 	}
-	
+
 	public void selectFoul() {
-		
+
 	}
-	
+
 	public void selectStadium() {
-		
+
 	}
 
 }
